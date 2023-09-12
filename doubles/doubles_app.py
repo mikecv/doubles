e3fe1bf366dc6daa98bfda74f7fc80feae7ab646
mike <galacticwingcommander@gmail.com>
2023-09-12 20:32:49 +0800

    Changed to python 3.9.9 min for bash terminal. Removed references to main gile.

--- doubles/doubles_app.py
+++ doubles/doubles_app.py
@@ -3,10 +3,7 @@ Text analysis to find duplicate questions.
 """
 
 import click
-import csv
 import logging
-import os
-import sys
 
 import dotsi  # type: ignore
 
@@ -23,7 +20,7 @@ class Doubles:
     Main class the question analyser application.
     """
 
-    def __init__(self, csv_file):
+    def __init__(self, q_file):
         """
         Doubles initialisation.
         """
@@ -41,15 +38,15 @@ class Doubles:
         log.info(f"Initialising application: {self._app_name}, version: {self._app_version}")
 
         # Optional include file.
-        if csv_file is None:
+        if q_file is None:
             log.warning("No questions file specified.")
         else:
-            log.info(f"Question file to process: {csv_file}.")
+            log.info(f"Question file to process: {q_file}.")
 
             # Read the questions into the questions store
-            self.questions = Question_Store(csv_file, self._settings)
+            self.questions = Question_Store(q_file, self._settings)
             if self.questions.num_q == 0:
-                log.warning("No questions in CSV file, exiting...")
+                log.warning("No questions in question file, exiting...")
                 exit(0)
 
             # Process the questions.
@@ -60,8 +57,8 @@ class Doubles:
 
 
 @click.command()
-@click.option("-q", "--csv-file", type=click.Path(exists=False), help="Path to the CSV file")
-def run(csv_file) -> None:
+@click.option("-q", "--q-file", type=click.Path(exists=False), help="Path to the .xlsx file")
+def run(q_file) -> None:
     """
     Poetry calls this to get the application up and running.
     Assumes a python script in project.toml as follows:
@@ -72,7 +69,7 @@ def run(csv_file) -> None:
     Can then run as: poetry run doubles-go
     """
 
-    Doubles(csv_file)
+    Doubles(q_file)
 
 
 if __name__ == "__main__":
